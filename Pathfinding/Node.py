daf7ce8f34dd7365326a61f730cb177aff8799f4
Monkvy <moluecke@gmx.de>
2022-06-07 14:32:08 +0200

    Commenting

--- Pathfinding/Node.py
+++ Pathfinding/Node.py
@@ -3,11 +3,18 @@ import pygame
 
 class Node:
     def __init__(self):
+        '''
+        The Node class is the class that represents a single node in the grid.
+        '''
+
         self.type = 'none'
         self.neighbours = []
         self.g_score = 0
         self.h_score = 0
         self.f_score = 0
+
+    def closed(self):
+        pass
 
     def draw(self, surface: pygame.Surface):
         pass
--- Pathfinding/algorithms/a_star.py
+++ Pathfinding/algorithms/a_star.py
@@ -1,9 +1,17 @@
 import pygame
-from Node import Node
+from node import Node
 
 
 class AStar:
     def __init__(self, grid: list[list[Node]]):
+        '''
+        The AStar class is the class that implements the A* algorithm.
+        It is used to find the shortest path between two nodes.
+
+        Args:
+            * grid (list[list[Node]]) - The grid of nodes.
+        '''
+
         self.grid = grid
 
     def update(self):
