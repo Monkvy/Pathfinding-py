b304780ecc295573bbd5bbd2f330d233188eb838
Monkvy <moluecke@gmx.de>
2022-06-07 14:22:32 +0200

    Node class

--- Pathfinding/Node.py
+++ Pathfinding/Node.py
@@ -4,9 +4,9 @@ import pygame
 class Node:
     def __init__(self):
         self.type = 'none'
-        self.g_cost = 0
-        self.h_cost = 0
-        self.f_cost = 0
+        self.g_score = 0
+        self.h_score = 0
+        self.f_score = 0
 
     def draw(self, surface: pygame.Surface):
         pass
